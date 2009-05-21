c8f2e6cccce338b930500f0c66d593b10a3d9a6d
Leah Culver <leah.culver@gmail.com>
2009-05-21 23:26:56 +0000

    optional trailing slash on urls

--- typepadapp/urls.py
+++ typepadapp/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import *
 
 # auth pages
 urlpatterns = patterns('typepadapp.views.auth',
-    url(r'^login$', 'login', name='login'),
-    url(r'^register$', 'register', name='register'),
+    url(r'^login/?$', 'login', name='login'),
+    url(r'^register/?$', 'register', name='register'),
     url(r'^authorize/?$', 'authorize', name='authorize'),
     url(r'^logout/?$', 'logout', name='logout'),
     url(r'^synchronize/?$', 'synchronize', name='synchronize'),
