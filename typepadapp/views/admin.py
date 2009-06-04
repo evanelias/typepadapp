3ffeafb9dcc4c8fa6178457c0ed22d2a9577feae
Brad Choate <brad@bradchoate.com>
2009-06-04 18:30:18 +0000

    Changes to preserve entire membership object during export; eventually the creation date will be gathered from the membership record and included in the export.

--- typepadapp/views/admin.py
+++ typepadapp/views/admin.py
@@ -46,8 +46,7 @@ def generate_members_csv(request):
     if request.user.is_superuser:
 
         # convert to user list
-        members = [member.source for member in members]
-        ids = [member.id for member in members] # xids of members, not needed if cmp did user ids
+        ids = [member.source.id for member in members] # xids of members, not needed if cmp did user ids
 
         # output csv
         mfile = get_members_csv(members)
@@ -55,7 +54,7 @@ def generate_members_csv(request):
 
         # wiggle room
         new_offset = len(ids) - 4
-        
+
         # more pages of members
         while new_offset > offset:
 
@@ -74,9 +73,9 @@ def generate_members_csv(request):
             members = []
             for m in more:
                 if m.source.id not in ids: # remove dupes
-                    members.append(m.source)
+                    members.append(m)
                     ids.append(m.source.id)
-            
+
             # output csv
             mfile = get_members_csv(members)
             yield mfile.getvalue()
@@ -88,10 +87,14 @@ def get_members_csv(members):
     mfile = StringIO.StringIO()
     writer = csv.writer(mfile)
 
-    for member in members:
-
+    for membership in members:
+        member = membership.source
+        # TODO: add creation date to the export; date is in membership.status.created
         # member data from typepad
-        member_data = [member.id, member.display_name, member.email, member.gender, member.location, member.about_me, member.homepage, ' '.join(member.interests)]
+        member_data = [member.xid,
+            member.display_name, member.email,
+            member.gender, member.location, member.about_me,
+            member.homepage, ', '.join(member.interests)]
         row = []
         for item in member_data:
             if item:
@@ -106,7 +109,7 @@ def get_members_csv(members):
             for field in profile_form:
                 value = typepadapp.templatetags.formfieldvalue.value_of_field(field)
                 row.append(value)
-        
+
         writer.writerow(row)
 
     return mfile
