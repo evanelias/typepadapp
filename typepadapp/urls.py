8b93a8ed95784259de5e22ecdeb6fd901eac7759
Leah Culver <leah.culver@gmail.com>
2009-06-05 23:40:26 +0000

    Added common css and js to typepadapp.

--- typepadapp/urls.py
+++ typepadapp/urls.py
@@ -1,4 +1,8 @@
+import os.path
 from django.conf.urls.defaults import *
+
+app_path = os.path.dirname(__file__)
+media_dir = os.path.join(app_path, 'static')
 
 # auth pages
 urlpatterns = patterns('typepadapp.views.auth',
@@ -11,4 +15,9 @@ urlpatterns = patterns('typepadapp.views.auth',
 
 urlpatterns += patterns('typepadapp.views.admin',
     url(r'^admin/export/members/?$', 'export_members', name='export_members'),
+)
+
+urlpatterns += patterns('',
+    url(r'^static/typepadapp/(?P<path>.*)/?$', 'django.views.static.serve',
+        kwargs={ 'document_root': media_dir }),
 )
