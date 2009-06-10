02bd614a4afb82067b231b8eb0eda8380b59e4c5
Mike Malone <mjmalone@gmail.com>
2009-06-10 18:09:59 +0000

    moved database and frontend url configuration options to local_settings.py

--- typepadapp/conf/project_template/settings.py
+++ typepadapp/conf/project_template/settings.py
@@ -22,13 +22,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'                # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '{{ project_name }}.db'    # Or path to database file if using sqlite3.
-DATABASE_USER = ''                         # Not used with sqlite3.
-DATABASE_PASSWORD = ''                     # Not used with sqlite3.
-DATABASE_HOST = ''                         # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''                         # Set to empty string for default. Not used with sqlite3.
-
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = ''
 
