c9fd75bea49c5bc40e517e4013e4917cc8bf9ed1
Brad Choate <brad@bradchoate.com>
2009-06-30 20:54:55 +0000

    Register custom 404 handler.

--- typepadapp/conf/project_template/urls.py
+++ typepadapp/conf/project_template/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 
 handler500 = 'motion.views.handle_exception'
+handler404 = 'motion.views.handle_not_found'
 
 urlpatterns = patterns('',
     (r'^', include('motion.urls')),
