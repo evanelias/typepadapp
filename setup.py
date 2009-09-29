f66a04ab1d0e1a6778d555638246752d077fa8a2
Mark Paschal <markpasc@markpasc.org>
2009-09-29 15:29:58 -0700

    Point URL at github, not made-up subversion repo

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     description='Base for TypePad cloud apps',
     author='Six Apart',
     author_email='python@sixapart.com',
-    url='http://code.sixapart.com/svn/typepadapp/',
+    url='http://github.com/sixapart/typepadapp',
 
     long_description=open(join(dirname(__file__), 'README.rst')).read(),
     classifiers=[
