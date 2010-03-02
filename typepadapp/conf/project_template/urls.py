88bd03e16bf32cd3e631caa93aaeed41e327e879
Mark Paschal <markpasc@markpasc.org>
2010-03-02 15:08:32 -0800

    Use typepadapp's urls first (as replacing motion with another app might override the sign-in urls otherwise)

--- typepadapp/conf/project_template/urls.py
+++ typepadapp/conf/project_template/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 from typepadapp.urls import handler500, handler404
 
 urlpatterns = patterns('',
-    (r'^', include('motion.urls')),
     (r'^', include('typepadapp.urls')),
+    (r'^', include('motion.urls')),
 )
